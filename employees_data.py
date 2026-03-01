26993c2c6fc217952011826a9619c88ce4bad8af
Spandana.K <spandanakorlam@gmail.com>
2026-03-01 19:15:27 +0530

    Add files via upload

---
+++ employees_data.py
@@ -0,0 +1,7 @@
+employees = [
+    {"id": 1, "name": "Sanju", "dept": "IT", "salary": 50000},
+    {"id": 2, "name": "Ravi", "dept": "HR", "salary": 30000},
+    {"id": 3, "name": "Anu", "dept": "IT", "salary": 45000},
+    {"id": 4, "name": "Kiran", "dept": "Finance", "salary": 60000},
+    {"id": 5, "name": "Meena", "dept": "HR", "salary": 40000}
+]
